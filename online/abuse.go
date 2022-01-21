daf276a6a49424d8a59c81c29109fbee582439c7
Finwë <finwe@finwe.tf>
2022-01-21 08:31:43 +0100

    online: fetch only unresolved abuses

--- online/abuse.go
+++ online/abuse.go
@@ -5,23 +5,23 @@ import (
 	"fmt"
 )
 
-// Abuse Representation from Online.net's API
+// Abuse Representation from Online.net's API, limited to what we need
 type Abuse struct {
-	ID          int    `json:"id"`
-	Date        string `json:"date"`
-	Status      string `json:"status"`
-	SentDate    string `json:"sent_date"`
-	Description string `json:"description"`
-	Sender      string `json:"sender"`
-	Service     string `json:"service"`
-	Category    string `json:"type"`
+	ID       string `json:"id"`
+	Date     string `json:"date"`
+	Status   string `json:"status"`
+	Sender   string `json:"sender"`
+	Service  string `json:"service"`
+	Category string `json:"type"`
 }
 
-// GetAbuses Retrieves all the available abuses of an Online.net account
+// GetAbuses Retrieves all the pending abuses of an Online.net account
 func GetAbuses() ([]Abuse, error) {
 	var abuses []Abuse
 
-	body, err := fetch("abuse")
+	// We fetch only the first page of *unresolved* abuses
+	// If you have more than one page... what are you doing on the Internet?
+	body, err := fetch("abuse?status=pending")
 	if err != nil {
 		return abuses, err
 	}

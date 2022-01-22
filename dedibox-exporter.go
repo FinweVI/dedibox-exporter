71fd0fcfd68b55d8141d040f6d68a992290727f0
Finwë <finwe@finwe.tf>
2022-01-22 14:35:35 +0100

    improve logging when no collector is selected

--- dedibox-exporter.go
+++ dedibox-exporter.go
@@ -32,7 +32,8 @@ func main() {
 
 	if len(myCollectors) == 0 {
 		myCollectors = append(myCollectors, "abuse")
-		log.Debug("No collector selected, using default configuration")
+		log.WithField("collectors", myCollectors).
+			Debug("No collector selected, using default configuration")
 	}
 
 	for _, cltr := range myCollectors {
